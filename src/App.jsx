2f3f62a2bd61b9959960963744fc2915488cba5a
Kelvin F. Baez Rogriguez <kelvin12378945610@gmail.com>
2022-09-17 14:30:55 -0400

    [add] tailwind config colorsa and fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Insure landing page</h1>;
+  return (
+    <>
+      <h1 className="text-3xl text-grayish-blue font-heading">
+        Insure landing page 1
+      </h1>
+
+      <h1 className="text-3xl text-dark-violet font-body">
+        Insure landing page 2
+      </h1>
+    </>
+  );
 }
 
 export default App;
