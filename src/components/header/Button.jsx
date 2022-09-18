a0991cca6c13871eadfe546021f959e77bf21d2c
Kelvin F. Baez Rogriguez <kelvin12378945610@gmail.com>
2022-09-18 19:47:21 -0400

    [add] className uppercase button component

--- src/components/header/Button.jsx
+++ src/components/header/Button.jsx
@@ -1,7 +1,7 @@
 export const Button = () => {
   return (
-    <button className="transition-all font-bold hover:text-dark-violet hover:bg-very-light-gray  text-very-light-gray py-2 px-8 mb-24 border-very-light-gray border-solid border-2 font-body relative z-10">
-      VIEW PLANS
+    <button className="transition-all font-bold hover:text-dark-violet hover:bg-very-light-gray text-very-light-gray py-2 px-8 mb-24 border-very-light-gray border-solid border-2 font-body relative z-10 uppercase">
+      view plans
     </button>
   );
 };
