ff39c2515a8b0156165894dfc1d113a824a2c600
Kelvin F. Baez Rogriguez <kelvin12378945610@gmail.com>
2022-09-17 20:48:59 -0400

    [add] Banner componets

---
+++ src/components/header/Banner.jsx
@@ -0,0 +1,12 @@
+export const Banner = () => {
+  return (
+    <div>
+      <img src="" alt="" />
+      <div>
+        <h2></h2>
+        <p>Get your life insurance coverage easier and faster.</p>
+        <button>VIEW PLANS</button>
+      </div>
+    </div>
+  );
+};
