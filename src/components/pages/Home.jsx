89245fa29bd685488477a4f07595e32d9dd3bb33
Cristian Perez <87153751+PerezCristian-dev@users.noreply.github.com>
2022-09-20 21:16:22 -0400

    [add] Pages and Routes

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { Body, Footer, Header } from "../../components";
+
+function App() {
+  return (
+    <>
+      <Header className="font-body" />
+      <Body className="font-body" />
+      <Footer className="font-body" />
+    </>
+  );
+}
+
+export default App;
