d6a30872932086399a086ce1d205110ad6612d1c
Cristian Perez <87153751+PerezCristian-dev@users.noreply.github.com>
2022-09-23 02:12:38 -0400

    Update FooterSection.jsx

--- src/components/footer/FooterSection.jsx
+++ src/components/footer/FooterSection.jsx
@@ -13,28 +13,28 @@ export const FooterSection = () => {
   return (
     <div className="flex flex-col footer-bg-mb lg:footer-bg-dk  bg-very-light-gray bg-local items-center">
       <div className="w-screen p-28">
-      <div className="flex flex-col items-center lg:flex-row lg:justify-between lg:w-full">
-        <img src={logo} alt="" className="mt-20 w-36 lg:m-0" />
-        <div className="flex flex-row m-4 pt-2 lg:m-0 lg:items-center">
-          <img src={facebook} alt="instagram" className="m-2 w-8" />
-          <img src={twitter} alt="facebook" className="m-2 w-8" />
-          <img src={pinterest} alt="facebook" className="m-2 w-8" />
-          <img src={instagram} alt="facebook" className="m-2 w-8" />
+        <div className="flex flex-col items-center lg:flex-row lg:justify-between lg:w-full">
+          <img src={logo} alt="" className="mt-20 w-36 lg:m-0" />
+          <div className="flex flex-row m-4 pt-2 lg:m-0 lg:items-center">
+            <img src={facebook} alt="instagram" className="m-2 w-8" />
+            <img src={twitter} alt="facebook" className="m-2 w-8" />
+            <img src={pinterest} alt="facebook" className="m-2 w-8" />
+            <img src={instagram} alt="facebook" className="m-2 w-8" />
+          </div>
         </div>
-      </div>
 
-      <hr className="border-1 border-dark-grayish-violet mb-4 mt-10"/>
-      <span className="bg-dark-grayish-violet w-11/12 h-px my-5"></span>
+        <hr className="border-1 border-dark-grayish-violet mb-4 mt-10" />
+        <span className="bg-dark-grayish-violet w-11/12 h-px my-5"></span>
 
-      <div className="text-center lg:flex lg:justify-between lg:w-full lg:text-left">
-        {MenuItems.map((item) => (
-          <FooterMenu
-            key={item.id}
-            MenuItems={item.menuItems}
-            MenuTitle={item.menuTitle}
-          />
-        ))}
-      </div>
+        <div className="text-center lg:flex lg:justify-between lg:w-full lg:text-left">
+          {MenuItems.map((item) => (
+            <FooterMenu
+              key={item.id}
+              MenuItems={item.menuItems}
+              MenuTitle={item.menuTitle}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
