293b1364358c15ccd4b333d20aaaeeac7288c7e1
Kelvin F. Baez Rogriguez <kelvin12378945610@gmail.com>
2022-10-01 15:39:44 -0400

    [update] BannerBody component - add Navlink

--- src/components/body/BannerBody.jsx
+++ src/components/body/BannerBody.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import { Button } from "../header/Button";
 import { BgPattern } from "./BgPattern";
 
@@ -8,7 +9,10 @@ export const BannerBody = () => {
       <h2 className="text-very-light-gray text-5xl font-headings pb-10 lg:text-left lg:w-2/4 z-10">
         Find out more about how we work
       </h2>
-      <Button btnName="how we work" />
+
+      <NavLink to="/how-we-work">
+        <Button btnName="how we work" />
+      </NavLink>
     </div>
   );
 };
